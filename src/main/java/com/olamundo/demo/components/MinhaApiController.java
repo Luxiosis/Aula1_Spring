3402d7cd6c698eb5afa7a56766bca042b3b1b003
Luxiosis <lucasdudu1001@gmail.com>
2023-03-01 14:31:35 -0300

    Initial commit

---
+++ src/main/java/com/olamundo/demo/components/MinhaApiController.java
@@ -0,0 +1,12 @@
+package com.olamundo.demo.components;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MinhaApiController {
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "Hello World";
+    }
+}
